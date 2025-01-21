f1fade65bdc4c925db32273ab204d095ad953a2e
Damian Eisenring <eisenringdamian@gmail.com>
2025-01-21 10:38:50 +0100

    Added java project

---
+++ RestAPI_BloggingPage/src/main/java/com/example/restapi_bloggingpage/RestApiBloggingPageApplication.java
@@ -0,0 +1,14 @@
+package com.example.restapi_bloggingpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiBloggingPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiBloggingPageApplication.class, args);
+
+    }
+
+}
